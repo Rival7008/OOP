6ada8f66bb74bd9ab1f6c4acc1eec1209c303dd9
rival7008 <antonasberg9@gmail.com>
2022-10-28 19:53:48 +0200

    Initial commit

---
+++ OOP/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Circle
+    {
+        float _pi = 3.141f;
+        int _Radius;
+        public Circle(int radius)
+        {
+            _Radius = radius;
+        }
+        public float getArea()
+        {
+            return _Radius * _Radius * _pi;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle CircleArea = new Circle(5);
+            Console.WriteLine(CircleArea.getArea());
+            Circle CircleArea2 = new Circle(6);
+            Console.WriteLine(CircleArea2.getArea());
+        }
+    }
+}
